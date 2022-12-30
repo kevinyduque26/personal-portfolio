c53ba76e950967d156cc96814db1c46104a4c023
kevinduque26 <kevinyduque26@gmail.com>
2022-12-29 21:29:50 -0600

    Added links and additional styling to hero

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,8 @@ import "./Footer.css"
 function Footer() {
     return (
         <div className="footer-container">
-            <p>Designed, developed, and deployed by Kevin Duque</p>
+            <p>Designed, developed, and deployed by Kevin Duque.</p>
+
         </div>
     )
 }
--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -3,17 +3,17 @@ import React from "react";
 import "./Hero.css"
 import picture from "./picture.jpg"
 
-
 function Hero() {
     return (
         <div className="hero-container stacked">
 
             <div className="hero-container-left-col">
                 <div className="hero-container-left-col-content">
-                    <h1><span style={{color: "#1EC5F5"}}>Visionary</span> that builds products & produces music</h1>
-                    <p>I'm Kevin Duque, a creative and versatile product manager based in Houston, TX. I'm most known for driving clarity and setting a vision in complex situations; oh, and producing music.</p>
-                </div>
-                
+                    <p className="eyebrow">KEVIN DUQUE</p>
+                    <h1>I'm a <span style={{color: "#1EC5F5"}}>visionary</span> that builds products & creates content.</h1>
+                    <p>Currently, I'm a remote senior product manager with <a className="text-link" target="_blank" href="https://www.bynder.com/en/" rel="noopener noreferrer">Bynder</a> based in Houston, TX. I'm most known for driving clarity and setting a vision in complex situations; oh, and making music.</p>
+                    <a className="button" target="_blank" href="mailto:kevinyduque26@gmail.com" rel="noopener noreferrer">Say Hello</a>
+                </div>                
             </div>
 
             <div className="hero-container-right-col">
