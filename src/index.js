ead4c3b0f47203706f9e26fc3b0e1ac5857fd21e
kevinduque26 <kevinyduque26@gmail.com>
2022-12-31 16:08:54 -0600

    Changes to react version

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+const root = document.getElementById("root")
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  root
 );
