bacce2b77b33f2583b4c97acb4460a97e440cd4c
kevinduque26 <kevinyduque26@gmail.com>
2022-12-31 16:12:04 -0600

    Testing after downgrading react version

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navigation from './Navigation/Navigation';
 
 import Hero from './Hero/Hero';
 import QuicklinksMain from './QuicklinksMain/QuicklinksMain';
-// import MusicSpotlight from './MusicSpotlight/MusicSpotlight';
+import MusicSpotlight from './MusicSpotlight/MusicSpotlight';
 import LanguageTicker from './LanguageTicker/LanguageTicker';
-// import Testing from './Testing/Testing';
+import Testing from './Testing/Testing';
 import Footer from './Footer/Footer';
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
         <div className="app-body">
           <Hero />    
           <QuicklinksMain />
-          {/* <MusicSpotlight /> */}
+          <MusicSpotlight />
           <LanguageTicker />
-          {/* <Testing /> */}
+          <Testing />
         </div>
 
       <Footer />
---
+++ src/Testing/Testing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Testing() {
+    return null;
+};
+
+export default Testing;
